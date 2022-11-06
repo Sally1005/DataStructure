f55757960a9b8a7f2346d5ea0ed807b5639a5731
Sally1005 <1564358705@qq.com>
2022-11-06 23:20:32 +0800

    修改节点类

---
+++ src/main/java/com/lonton/tree/pojo/TreeNode.java
@@ -0,0 +1,57 @@
+package com.lonton.tree.pojo;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+/**
+ *节点类
+ *
+ * @author 张利红
+ * date:2022-11-05
+ */
+
+@Data
+@Slf4j
+public class TreeNode<T> {
+    /**
+     * 当前节点
+     */
+    private T data;
+
+    /**
+     * 左子节点
+     */
+    private TreeNode left;
+
+    /**
+     * 右子节点
+     */
+    private TreeNode right;
+
+    /**
+     * id
+     */
+    private int id;
+
+    /**
+     * 父id
+     */
+    private int parentID;
+
+    /**
+     * 无参构造
+     */
+    public TreeNode() {
+    }
+
+    /**
+     * 有参构造
+     * @param data
+     * @param id
+     * @param parentID
+     */
+    public TreeNode(T data, int id, int parentID) {
+        this.data = data;
+        this.id = id;
+        this.parentID = parentID;
+    }
+}
