7ecc56e9c0a72b83aa895a8d1de0bc7ef4fbfa5d
Sally1005 <1564358705@qq.com>
2022-11-06 23:28:31 +0800

    二叉树访问数据接口

--- src/main/java/com/lonton/tree/IVisitor.java
+++ src/main/java/com/lonton/tree/IVisitor.java
@@ -1,8 +1,17 @@
 package com.lonton.tree;
 
+import com.lonton.tree.impl.BinaryTree;
+
 /**
- * 二叉树访问数据接口
+ *二叉树访问数据接口
+ *
  * @author 张利红
+ * date:2022-11-05
  */
 public interface IVisitor {
+    /**
+     * 访问树
+     * @param binaryTree
+     */
+    void visit(BinaryTree binaryTree);
 }
