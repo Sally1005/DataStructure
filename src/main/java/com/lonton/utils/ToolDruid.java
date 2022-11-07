7613411ba1c3c519b5d81636cb6d4023dd5d6a9a
Sally1005 <1564358705@qq.com>
2022-11-07 11:20:55 +0800

    修改数据库连接池

--- src/main/java/com/lonton/utils/ToolDruid.java
+++ src/main/java/com/lonton/utils/ToolDruid.java
@@ -1,49 +1,57 @@
 package com.lonton.utils;
 
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
 /**
- * 数据库连接池
+ *数据库连接池
+ *
  * @author 张利红
+ * date:2022-11-07
  */
+@Slf4j
 public class ToolDruid {
     private static DataSource dataSource;
-    static { // 在静态块中完成对 DataSource 的初始化
+
+    static { // 在静态代码块中对dataSource进行初始化
         Properties properties = new Properties();
+        properties.put("driverClassName","com.mysql.cj.jdbc.Driver");
+        properties.put("url","jdbc:mysql://localhost:3306/tree?serverTimezone=UTC&rewriteBatchedStatements=true");
+        properties.put("username","root");
+        properties.put("password","root");
         try {
-            properties.load(new FileInputStream("src//druid.properties"));
             dataSource = DruidDataSourceFactory.createDataSource(properties);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("获取连接池对象失败");
         }
     }
-    public static Connection connect() { // 获取 Connection 连接
+
+    public static Connection connect() {
         Connection connection = null;
         try {
             connection = dataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException throwables) {
+            log.error("数据库连接异常");
         }
         return connection;
     }
-    public static void close(ResultSet resultSet, Statement statement, Connection connection) { // 放回连接
-        try { // 判断是否为空
+
+    public static void close(ResultSet resultSet, Statement statement, Connection connection) {
+        try {
             if (resultSet != null)
                 resultSet.close();
             if (statement != null)
                 statement.close();
             if (connection != null)
                 connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException throwables) {
+            throw new RuntimeException(throwables);
         }
 
     }
