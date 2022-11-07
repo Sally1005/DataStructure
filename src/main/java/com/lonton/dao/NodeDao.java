da4ff22ea7cb7d9582a741233e2d332187723a48
Sally1005 <1564358705@qq.com>
2022-11-07 11:26:03 +0800

    修改节点DAO层

--- src/main/java/com/lonton/dao/NodeDao.java
+++ src/main/java/com/lonton/dao/NodeDao.java
@@ -1,16 +1,39 @@
 package com.lonton.dao;
 
-import com.lonton.domain.Node;
+import com.lonton.tree.pojo.TreeNode;
+import com.lonton.utils.ToolDruid;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import java.sql.Connection;
+import java.util.List;
 /**
- * NodeDao
+ * 节点DAO层
+ *
  * @author 张利红
+ * date:2022-11-07
  */
+public class NodeDao {
+    private final QueryRunner queryRunner = new QueryRunner();
 
-public class NodeDao extends BasicDao<Node> {
-    /**
-     *  1. ActorDAO 类中含有BasicDao 类中的所有方法
-     *  2. 可以根据业务逻辑添加新的方法
-     */
+    //查询多行
+    private List<TreeNode> queryMulti(String sql, Class<TreeNode> tClass, Object... parameters) {
+        Connection connection = null;
+        try {
+            connection = ToolDruid.connect();
+
+            List<TreeNode> list = queryRunner.query(connection, sql, new BeanListHandler<TreeNode>(tClass), parameters);
+            return list;
+        } catch (Exception throwables) {
+            throw new RuntimeException(throwables);
+        } finally {
+            ToolDruid.close(null, null, connection);
+        }
+    }
+
+    public List<TreeNode> getNodeList() {
+        return this.queryMulti("select * from fruit", TreeNode.class);
+    }
+
 
 }
