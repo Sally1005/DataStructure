a08f9a5fabb9ca9bcb86140afa619e9d967c93dd
Sally1005 <1564358705@qq.com>
2022-11-23 11:27:29 +0800

    IVisiter

--- src/main/java/com/lonton/binaryTree/tree/IVisitor.java
+++ src/main/java/com/lonton/binaryTree/tree/IVisitor.java
@@ -3,15 +3,17 @@ package com.lonton.binaryTree.tree;
 
 import com.lonton.binaryTree.tree.pojo.BinaryTree;
 
+import java.util.List;
+
 /**
- *二叉树访问数据接口
- *
+ * 二叉树访问数据接口
+ * <p/>
  * @author 张利红
  */
+@SuppressWarnings("all")
 public interface IVisitor {
     /**
      * 访问树
-     * @param binaryTree
      */
-    int visit(BinaryTree binaryTree);
+    void visit(BinaryTree.TreeNode root, List<Object> list);
 }
