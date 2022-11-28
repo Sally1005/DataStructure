c7ec56ed065b8c281ca68a49b79ef5a4a3a2c6b0
Sally1005 <1564358705@qq.com>
2022-11-28 16:48:34 +0800

    测试连接

---
+++ src/test/java/com/lonton/binarytree/MybatisCacheTests.java
@@ -0,0 +1,29 @@
+package com.lonton.binarytree;
+
+import com.lonton.binarytree.dao.TreeNodeMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Mybatis单元测试
+ * <p/>
+ * @author 张利红
+ */
+@Slf4j
+@SpringBootTest
+public class MybatisCacheTests {
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    void testCache() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        TreeNodeMapper treeNodeMapper = sqlSession.getMapper(TreeNodeMapper.class);
+        log.debug("执行查询……");
+        treeNodeMapper.list();}
+}
