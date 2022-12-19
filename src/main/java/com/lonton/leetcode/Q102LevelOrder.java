e958178a5dd807c8b191b158be386384487bb1be
Sally1005 <1564358705@qq.com>
2022-12-19 15:33:26 +0800

    Q102LevelOrder

---
+++ src/main/java/com/lonton/leetcode/Q102LevelOrder.java
@@ -0,0 +1,89 @@
+package com.lonton.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历 <p/>
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。<p/>
+ * 思路：<p/>
+ * 与之前写的递归遍历二叉树深度搜索和返回值为一维数组不同，<p/>
+ * 层序遍历为广度优先搜索，且示例的返回值为二维数组，<p/>
+ * 参考可以使用队列进行计数，大 list 里面 包含几个小 list (树有几层就有几个小 list )
+ * @author 张利红
+ */
+public class Q102LevelOrder {
+    /**
+     * 层序遍历
+     * @param root 根节点
+     * @return list
+     */
+    public List<List<Integer>> levelOrder(TreeNode root){
+        // 大 list
+        List<List<Integer>> bList = new LinkedList<>();
+        if(root==null){
+            return bList;
+        }
+         // 创建一个队列，并将根结点加入队列
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        // 当队列为空时，结束循环
+        while(!queue.isEmpty()){
+            // 求队列当前的大小
+            int size=queue.size();
+            List<Integer> list=new ArrayList<>();
+            // 每一层的数据个数
+            while(size>0) {
+                // 当队列里有元素时，就将该队列的首元素出队列
+                TreeNode cur = queue.poll();
+                if(cur!=null){
+                    // 就将cur里的元素放进list里
+                    list.add(cur.val);
+                    if(cur.left!=null){
+                        // 将cur.left入队列
+                        queue.offer(cur.left);
+                    }
+                    if(cur.right!=null){
+                        queue.offer(cur.right);
+                    }
+                }
+                // 小 list 中元素自减
+                size--;
+            }
+            // 将list里的元素放入大 list
+            bList.add(list);
+        }
+        return bList;
+    }
+
+
+
+    /**
+     * 定义二叉树 （题目给出条件）<br/>
+     * 提示：<br/>
+     * 两棵树上的节点数目都在范围 [0, 2000] 内<br/>
+     * -1000 <= Node.val <= 1000
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+
+    }
+}
