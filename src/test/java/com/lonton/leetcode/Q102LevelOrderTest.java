49ee36b3c43f47f75ad40303fd92464357dfdadb
Sally1005 <1564358705@qq.com>
2022-12-21 17:28:31 +0800

    修改

--- src/test/java/com/lonton/leetcode/Q102LevelOrderTest.java
+++ src/test/java/com/lonton/leetcode/Q102LevelOrderTest.java
@@ -1,12 +1,10 @@
 package com.lonton.leetcode;
 
+import cn.hutool.core.util.ArrayUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 
@@ -25,17 +23,16 @@ public class Q102LevelOrderTest {
                 new Q102LevelOrder.TreeNode(9),
                 new Q102LevelOrder.TreeNode(20, new Q102LevelOrder.TreeNode(15), new Q102LevelOrder.TreeNode(7))
         );
-
-        log.info("层序遍历树：{}",new Q102LevelOrder().levelOrder(treeNode));
-
-//        String expected = "[[3], [9, 20], [15, 7]]";
-//        String actual = new Q102LevelOrder().levelOrder(treeNode).toString();
-//        Assertions.assertSame(expected,actual,"NotEquals");
+        Integer arr[][] = {{3},{9,20},{15,7}};
+        List<List<Integer>> actual = new Q102LevelOrder().levelOrder(treeNode);
+        // cn.hutool.core.util.ArrayUtil:直接打印二维数组对象，而非对象地址
+        log.info("预期层序遍历树：{}",ArrayUtil.toString(arr));
+        log.info("实际层序遍历树：{}",actual);
+        Assertions.assertEquals(ArrayUtil.toString(arr),actual.toString(),"NotEquals");
 
 
 
 
     }
-
 
 }
