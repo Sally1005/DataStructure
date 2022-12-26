a556f9ab57d38ae444b6ddd992e22a7575a03ba1
Sally1005 <1564358705@qq.com>
2022-12-26 22:17:59 +0800

    将遍历树方法移动到抽象类中

--- src/main/java/com/lonton/binarytree/ATraverser.java
+++ src/main/java/com/lonton/binarytree/ATraverser.java
@@ -8,6 +8,16 @@ import com.lonton.binarytree.tree.pojo.BinaryTree;
  * @author 张利红
  */
 public abstract class ATraverser implements ITraverser {
+    /**
+     * 遍历树 (在具体的实现类中实现）
+     * @param tree 二叉树
+     * @param visitor 访问visitor
+     * @param param 可变参数
+     * @return 任意类型
+     */
+    public Object traverser(BinaryTree tree, IVisitor visitor,Object... param){
+        return traverser(tree,visitor,param);
+    }
 
     /**
      * 访问树
