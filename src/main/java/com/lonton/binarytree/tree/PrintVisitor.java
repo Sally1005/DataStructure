6bb78820351b2b5c8b21f8285c826cbcf3b52dd9
Sally1005 <1564358705@qq.com>
2022-12-26 11:04:02 +0800

    visitor实现类，打印树

---
+++ src/main/java/com/lonton/binarytree/tree/PrintVisitor.java
@@ -0,0 +1,38 @@
+package com.lonton.binarytree.tree;
+
+import com.lonton.binarytree.tree.pojo.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 打印
+ * <p/>
+ * @author 张利红
+ */
+public class PrintVisitor implements IVisitor {
+    /**
+     * 树中数据集合
+     */
+    private List<String> res = new ArrayList<>();
+
+    /**
+     * 打印二叉树
+     * @param node 根节点
+     * @param param 任意类型
+     * @return 树中数据
+     */
+    @Override
+    public Object visit(BinaryTree.TreeNode node, Object... param) {
+        res.add(node.getData());
+        return node;
+    }
+
+    /**
+     * 获取树中数据
+     * @return 树中数据
+     */
+    public List<String> getRes() {
+        return res;
+    }
+}
