95e61aefc083e9c4ee290e693b5f68b8fff59ab0
Sally1005 <1564358705@qq.com>
2022-12-31 20:01:47 +0800

    Q103ZigzagLevelOrderTest

---
+++ src/test/java/com/lonton/leetcode/Q103ZigzagLevelOrderTest.java
@@ -0,0 +1,39 @@
+package com.lonton.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * 测试二叉树的锯齿形层级遍历 <p/>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[20,9],[15,7]]
+ * @author 张利红
+ */
+@Slf4j
+public class Q103ZigzagLevelOrderTest {
+    @Test
+    public void test() {
+        // 构建二叉树
+        Q103ZigzagLevelOrder.TreeNode treeNode = new Q103ZigzagLevelOrder.TreeNode(3,
+                new Q103ZigzagLevelOrder.TreeNode(9),
+                new Q103ZigzagLevelOrder.TreeNode(20, new Q103ZigzagLevelOrder.TreeNode(15), new Q103ZigzagLevelOrder.TreeNode(7))
+        );
+        List<List<Integer>> actual = new Q103ZigzagLevelOrder().zigzagLevelOrder(treeNode);
+        log.info("actual:{}",actual);
+
+        List<List<Integer>> expected = new ArrayList<>(
+                Arrays.asList(Arrays.asList(3), Arrays.asList(20, 9), Arrays.asList(15, 7)));
+        log.info("expected:{}",expected);
+
+        Assertions.assertEquals(expected,actual,"NotEquals");
+
+
+    }
+
+}
