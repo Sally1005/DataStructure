ab91dcf7d195a2c0e14501a28d336fe516705340
Sally1005 <1564358705@qq.com>
2023-02-06 10:53:34 +0800

    重载visit方法

--- src/main/java/com/lonton/binarytree/IVisitor.java
+++ src/main/java/com/lonton/binarytree/IVisitor.java
@@ -3,6 +3,8 @@ package com.lonton.binarytree;
 
 import com.lonton.binarytree.pojo.BinaryTree;
 
+import java.util.function.Predicate;
+
 /**
  * 二叉树访问数据接口
  * <p/>
@@ -17,5 +19,13 @@ public interface IVisitor {
      * @return 任意类型
      */
     Object visit(BinaryTree.TreeNode node, Object... param);
+
+    /**
+     * 访问树<重载过滤>
+     * @param node 节点
+     * @param predicate 预测(过滤输入内容是否符合条件)
+     * @return 任意类型
+     */
+    Object visit(BinaryTree.TreeNode node, Predicate<BinaryTree.TreeNode> predicate);
 }
 
