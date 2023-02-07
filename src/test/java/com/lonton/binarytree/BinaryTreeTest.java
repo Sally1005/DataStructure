f3e17faf207f5202f0b10187865ef291b82bc2a1
Sally1005 <1564358705@qq.com>
2023-02-07 16:08:39 +0800

    测试midFilter方法

--- src/test/java/com/lonton/binarytree/BinaryTreeTest.java
+++ src/test/java/com/lonton/binarytree/BinaryTreeTest.java
@@ -19,8 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+
 
 
 /**
@@ -53,13 +52,7 @@ public class BinaryTreeTest {
      */
     @Test
     public void pre() {
-        PreTraverser preTraverser = new PreTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        };
+        PreTraverser preTraverser = new PreTraverser();
         List<String> list = new ArrayList<>();
         PrintVisitor visitor = new PrintVisitor();
         preTraverser.doVisitor(BuildTree(), visitor);
@@ -78,15 +71,9 @@ public class BinaryTreeTest {
     /**
      * 中序遍历打印二叉树
      */
-    @Test
+   // @Test
     public void mid() {
-        MidTraverser midTraverser = new MidTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param){
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        };
+        MidTraverser midTraverser = new MidTraverser();
         PrintVisitor visitor = new PrintVisitor();
         midTraverser.doVisitor(BuildTree(), visitor);
         List<String> res = visitor.getRes();
@@ -108,13 +95,7 @@ public class BinaryTreeTest {
      */
     @Test
     public void post() {
-        PostTraverser postTraverser = new PostTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        };
+        PostTraverser postTraverser = new PostTraverser();
         PrintVisitor visitor = new PrintVisitor();
         List<String> res = visitor.getRes();
         postTraverser.doVisitor(BuildTree(), visitor);
@@ -138,13 +119,7 @@ public class BinaryTreeTest {
         int id = 1;
         SearchVisitor visitor = new SearchVisitor();
         visitor.setId(id);
-        new PreTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        }.doVisitor(BuildTree(), visitor,id);
+        new PreTraverser().doVisitor(BuildTree(), visitor,id);
         String target = visitor.getFoundNode().getData();
         log.info("前序遍历查找节点:{}",target);
         Assertions.assertEquals("水果", target,"根据id，前序遍历查找节点失败。");
@@ -158,23 +133,11 @@ public class BinaryTreeTest {
         int id = 3;
         SearchVisitor visitor = new SearchVisitor();
         visitor.setId(id);
-        new MidTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-
-        }.doVisitor(BuildTree(), visitor,id);
-        for (BinaryTree.TreeNode treeNode : visitor.getFilterNode()) {
-            log.debug("id大于等于2的节点:{}",treeNode.getData());
-        }
+        new MidTraverser().doVisitor(BuildTree(), visitor,id);
         String target = visitor.getFoundNode().getData();
         log.info("中序遍历查找节点:{}",target);
-
+        // id=7的节点在中序遍历查找目标节点后即停止查找
         Assertions.assertEquals("瓜果类", target,"根据id，中序遍历查找节点失败。");
-
-
     }
 
     /**
@@ -185,13 +148,7 @@ public class BinaryTreeTest {
         int id = 3;
         SearchVisitor visitor = new SearchVisitor();
         visitor.setId(id);
-        new PostTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        }.doVisitor(BuildTree(), visitor, id);
+        new PostTraverser().doVisitor(BuildTree(), visitor, id);
         String target = visitor.getFoundNode().getData();
         log.info("后序遍历查找节点:{}",target);
         Assertions.assertEquals("瓜果类", target,"根据id，后序遍历查找节点失败。");
@@ -200,16 +157,10 @@ public class BinaryTreeTest {
     /**
      * 计算树的高度
      */
-    @Test
+   // @Test
     public void countHeight(){
         CountHeightVisitor visitor = new CountHeightVisitor();
-        new PostTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        }.doVisitor(BuildTree(),visitor);
+        new PostTraverser().doVisitor(BuildTree(),visitor);
         Assertions.assertEquals(3,visitor.getHeight(),"计算树的高度失败。");
     }
 
@@ -219,13 +170,7 @@ public class BinaryTreeTest {
     @Test
     public  void  nodeNum(){
         NodeNumVisitor visitor = new NodeNumVisitor();
-        new PreTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        }.doVisitor(BuildTree(),visitor);
+        new PreTraverser().doVisitor(BuildTree(),visitor);
         Assertions.assertEquals(7,visitor.getCount(),"计算树的节点个数失败。");
     }
 
@@ -235,13 +180,7 @@ public class BinaryTreeTest {
     @Test
     public  void prePrint(){
         PrintVisitor visitor = new PrintVisitor();
-        new PreTraverser() {
-            @Override
-            public Object traverse(BinaryTree tree, IVisitor visitor, Object... param) {
-                recursive(tree.getRoot(),visitor);
-                return tree;
-            }
-        }.doVisitor(BuildTree(),visitor);
+        new PreTraverser().doVisitor(BuildTree(),visitor);
         List<String> list = new ArrayList<>();
         list.add("水果");
         list.add("柑橘类");
@@ -254,41 +193,58 @@ public class BinaryTreeTest {
         Assertions.assertEquals(list,visitor.getRes(),"前序遍历打印二叉树于失败。");
     }
 
-    public static List<BinaryTree.TreeNode> treeList;
-    static {
-        BinaryTree.TreeNode<Object>  fruit = new BinaryTree.TreeNode<>(1, "水果", 0, 0, 1);
-        BinaryTree.TreeNode<Object>  citrus = new BinaryTree.TreeNode<>(2, "柑橘类", 1,1 , 2);
-        BinaryTree.TreeNode<Object>  melon = new BinaryTree.TreeNode<>(3, "瓜果类", 1, 0, 2);
-        BinaryTree.TreeNode<Object>  orange = new BinaryTree.TreeNode<>(4, "橙子", 2, 1, 3);
-        BinaryTree.TreeNode<Object> woGan = new BinaryTree.TreeNode<>(5, "沃柑", 2, 0, 3);
-        BinaryTree.TreeNode<Object>  watermelon = new BinaryTree.TreeNode<>(6, "西瓜", 3, 1, 3);
-        BinaryTree.TreeNode<Object>  haMiMelon= new BinaryTree.TreeNode<>(7, "哈密瓜", 3, 0, 3);
-        treeList = Arrays.asList(fruit, citrus, melon, orange, woGan, watermelon, haMiMelon);
-    }
-
-
-
-    @Autowired
-    private TreeNodeMapper mapper;
     @Test
-    public void testFilterByStream(){
-        List<BinaryTree.TreeNode> list = mapper.list();
+    public void testFilterByStream() {
+        List<BinaryTree.TreeNode> list = treeNodeMapper.list();
         list.stream() // 1) 创建流-> 1. 单列集合：collection、set 2. 双列集合：先map.entrySet/keySet转成单列集合
-                      // 3.数组：Arrays.stream(数组名)或者Stream.of(数组)
+                // 3.数组：Arrays.stream(数组名)或者Stream.of(数组)
                 .distinct() // 去重
-                .sorted((o1, o2) -> o2.getId()-o1.getId()) // 按照id进行降序排序
-                .filter(treeNode -> treeNode.getIsLeft()==0) // 过滤
+                .sorted((o1, o2) -> o2.getId() - o1.getId()) // 按照id进行降序排序
+                .filter(treeNode -> treeNode.getIsLeft() == 0) // 过滤
                 .limit(3) // 限制流的长度，多的部分将会被抛弃
                 .skip(1) // 跳过前1个元素（最大、最小）
-                .forEach(treeNode -> log.info("为左节点的节点：{}",treeNode.getData()));
-
-
+                .forEach(treeNode -> log.info("为左节点的节点：{}", treeNode.getData()));
     }
 
+    /**
+     * 中序测试过滤
+     */
     @Test
-    public void testFilter() {
+    public void midFilter(){
+        SearchVisitor visitor = new SearchVisitor();
+        visitor.setPredicate(treeNode -> treeNode.getId()>3);
+        new MidTraverser().doVisitor(BuildTree(), visitor);
+        List<String> actual = new ArrayList<>();
+        for (BinaryTree.TreeNode treeNode : visitor.getFilterNode()) {
+            actual.add(treeNode.getData());
+        }
+        List<String> list= new ArrayList<>();
+        list.add("橙子");
+        list.add("沃柑");
+        list.add("西瓜");
+        list.add("哈密瓜");
+        log.info("中序过滤二叉树:{}",list);
+        Assertions.assertEquals(list,actual,"中序过滤二叉树失败。");
 
-       // Arrays.asList("1","2","3","4").stream().filter()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
