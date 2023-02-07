0b5d6d3b5b8bfbb991d645219468f951a8abfa52
Sally <1564358705@qq.com>
2023-02-07 16:40:33 +0800

    移动traverse方法

--- src/main/java/com/lonton/binarytree/ITraverser.java
+++ src/main/java/com/lonton/binarytree/ITraverser.java
@@ -8,6 +8,14 @@ import com.lonton.binarytree.pojo.BinaryTree;
  * @author 张利红
  */
 public interface ITraverser {
+    /**
+     * 遍历树 (在具体的实现类中实现）
+     * @param tree 二叉树
+     * @param visitor 访问visitor
+     * @param param 可变参数
+     * @return 任意类型
+     */
+    Object traverse(BinaryTree tree, IVisitor visitor,Object... param);
 
     /**
      * 访问树 (在抽象类中实现）
@@ -16,6 +24,7 @@ public interface ITraverser {
      * @param param 可变参数
      * @return 任意类型
      */
-    Object doVisitor(BinaryTree binaryTree, IVisitor visitor, Object... param);
-
+    default Object doVisitor(BinaryTree binaryTree, IVisitor visitor, Object... param){
+        return traverse(binaryTree, visitor, param);
+    }
 }
