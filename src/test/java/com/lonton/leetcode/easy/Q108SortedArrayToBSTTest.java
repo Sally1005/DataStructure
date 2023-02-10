f7b98548fa581339d07e3ff5c1a35b8c32d32eb9
Sally <1564358705@qq.com>
2023-02-10 15:19:11 +0800

    Q108SortedArrayToBSTTest

--- src/test/java/com/lonton/leetcode/easy/Q108SortedArrayToBSTTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q108SortedArrayToBSTTest.java
@@ -1,54 +1,52 @@
 package com.lonton.leetcode.easy;
 
 
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 /**
  * 测试 将有序数组转换为二叉搜索树<p/>
- * 输入：nums = [-10,-3,0,5,9]<p/>
- * 输出：[0,-3,9,-10,null,5]<p/>
- * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案。<p/>
+ * 输入：nums = [-10,-3,0,5,9],result = [0,-10,5,null,-3,null,9]<p/>
+ * 比较两个有序数组是否构为相同的二叉搜索树
+ *
  * @author 张利红
  */
 @Slf4j
 public class Q108SortedArrayToBSTTest {
-
     @Test
-    public void test(){
+    public void test() {
         int[] nums = new int[]{-10, -3, 0, 5, 9};
-        Q108SortedArrayToBST.TreeNode actual = new Q108SortedArrayToBST().sortedArrayToBST(nums);
-        // 构树
-        Q108SortedArrayToBST.TreeNode expected = new Q108SortedArrayToBST.TreeNode(0);
-        expected.left = new Q108SortedArrayToBST.TreeNode(-10);
-        expected.left.right = new Q108SortedArrayToBST.TreeNode(-3);
-        expected.right = new Q108SortedArrayToBST.TreeNode(5);
-        expected.right.right = new Q108SortedArrayToBST.TreeNode(9);
+        TreeNode actual = new Q108SortedArrayToBST().sortedArrayToBST(nums);
 
-        assertTrue(isSameTree(expected, actual));
+        Integer[] result = new Integer[]{0, -10, 5, null, -3, null, 9};
+        TreeNode expected = BinaryTree.arrayToTree(result);
+
+        Assertions.assertTrue(isSameTree(expected, actual), "有序数组转换为二叉搜索树失败!");
 
     }
 
     /**
      * 判断是否为相同的树  复制 Q100
+     *
      * @param p 根节点 p
      * @param q 根节点 q
      * @return boolean
      */
-    private boolean isSameTree(Q108SortedArrayToBST.TreeNode p, Q108SortedArrayToBST.TreeNode q) {
-         // 如果两棵树的根节点都为空，那么它们为相同的树
-                 if(p == null && q == null){
-                     return true;
-                 }
-                   // 如果两棵树要么一棵树根节点为空，要么另一棵树根节点不为空，不是相同的树（上面已对都为空的情况进行判断）
-                 if(p == null || q == null){
-                     return false;
-                  }
-                  // 排除不相等的情况，再讨论两棵树相同情况下的条件
-               return (p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        // 如果两棵树的根节点都为空，那么它们为相同的树
+        if (p == null && q == null) {
+            return true;
+        }
+        // 如果两棵树要么一棵树根节点为空，要么另一棵树根节点不为空，不是相同的树（上面已对都为空的情况进行判断）
+        if (p == null || q == null) {
+            return false;
+        }
+        // 排除不相等的情况，再讨论两棵树相同情况下的条件
+        return (p.getVal() == q.getVal() && isSameTree(p.getLeft(), q.getLeft()) && isSameTree(p.getRight(), q.getRight()));
     }
 }
 
