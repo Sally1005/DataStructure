4393d2ab9a1e9f46efa1499a7363b5a6f674ec83
Sally <1564358705@qq.com>
2023-02-10 17:26:41 +0800

    Q112PathSumTest

--- src/test/java/com/lonton/leetcode/easy/Q112PathSumTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q112PathSumTest.java
@@ -1,35 +1,30 @@
 package com.lonton.leetcode.easy;
 
-import org.junit.Assert;
-import org.junit.Test;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * 测试路径总和 <p/>
- * 判断该树中是否存在 根节点到叶子节点 的路径。<p/>
- * 示例：<p/>
- * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22 <p/>
+ * 将数组 [5,4,8,11,null,13,4,7,2,null,null,null,1] 构建成二叉树,
+ * 并判断该树中是否存在 根节点到叶子节点 的路径 和为 t1 = 22, t2 = 27, t3 = 18, t4 = 26 <p/>
  * 输出：true
+ *
  * @author 张利红
  */
 public class Q112PathSumTest {
     @Test
     public void test() {
         // 构建二叉树
-        Q112PathSum.TreeNode root = new Q112PathSum.TreeNode(5,
-                new Q112PathSum.TreeNode(4,
-                        new Q112PathSum.TreeNode(11,
-                                new Q112PathSum.TreeNode(7),
-                                new Q112PathSum.TreeNode(2)),
-                        null),
-                new Q112PathSum.TreeNode(8,
-                        new Q112PathSum.TreeNode(13),
-                        new Q112PathSum.TreeNode(4,
-                                null,
-                                new Q112PathSum.TreeNode(1))
-                )
+        Integer[] nums = new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1};
+        TreeNode root = BinaryTree.arrayToTree(nums);
 
-        );
-
-        Assert.assertEquals("符合条件的路径总和不存在",true,Q112PathSum.hasPathSum(root, 22));
+        // 断言
+        Assertions.assertEquals(true, Q112PathSum.hasPathSum(root, 22), "NotEquals---符合条件的路径总和不存在。");
+        Assertions.assertEquals(true, Q112PathSum.hasPathSum(root, 27), "NotEquals---符合条件的路径总和不存在。");
+        Assertions.assertEquals(true, Q112PathSum.hasPathSum(root, 18), "NotEquals---符合条件的路径总和不存在。");
+        Assertions.assertEquals(true, Q112PathSum.hasPathSum(root, 26), "NotEquals---符合条件的路径总和不存在。");
     }
 }
