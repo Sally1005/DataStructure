d7a2eae5a628dc499347a1686076ff2bedec8428
Sally <1564358705@qq.com>
2023-02-10 14:19:39 +0800

    TreeNode(公共类）

---
+++ src/main/java/com/lonton/leetcode/common/TreeNode.java
@@ -0,0 +1,33 @@
+package com.lonton.leetcode.common;
+
+
+
+import lombok.Data;
+
+
+
+/**
+ * 定义树（题目给出条件）
+ *
+ * @author 张利红
+ */
+@Data
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+        this.val = val;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
