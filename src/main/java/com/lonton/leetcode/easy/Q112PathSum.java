a93a7a98964766a9e5951194cd8256e56d385394
Sally <1564358705@qq.com>
2023-02-10 17:25:30 +0800

    Q112PathSum

--- src/main/java/com/lonton/leetcode/easy/Q112PathSum.java
+++ src/main/java/com/lonton/leetcode/easy/Q112PathSum.java
@@ -1,4 +1,7 @@
 package com.lonton.leetcode.easy;
+
+import com.lonton.leetcode.common.TreeNode;
+
 /**
  * 112. 路径总和 <p/>
  * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。 <p/>
@@ -6,81 +9,49 @@ package com.lonton.leetcode.easy;
  * 如果存在，返回 true ；否则，返回 false 。<p/>
  * 叶子节点 是指没有子节点的节点。<p/>
  * 思路：
- * 递归计算每一条路径和，
- * 将路径总和和 targetSum 进行比较。
+ * 递归计算每一条路径和，将路径总和和 targetSum 进行比较。
+ *
  * @author 张利红
  */
-public class Q112PathSum {
+public class Q112PathSum extends TreeNode {
 
     /**
      * 判断是否有路径总和
-     * @param root 根节点
+     *
+     * @param root      根节点
      * @param targetSum 目标和
      * @return boolean
      */
     public static boolean hasPathSum(TreeNode root, int targetSum) {
-
-        // flag[0] = 0 路径总和不等  =1 有路径总和相等
+        // flag[0] = 0 路径总和不等  = 1 有路径总和相等
         int[] flag = {0};
         recurve(root, 0, targetSum, flag);
         return flag[0] == 1;
     }
 
-
     /**
      * 递归计算
+     *
      * @param treeNode  当前递归到的节点
-     * @param preSum  递归到当前节点之前的所有父节点之和
+     * @param preSum    递归到当前节点之前的所有父节点之和
      * @param targetSum 目标和
-     * @param flag  结果值
+     * @param flag      结果值
      */
     static void recurve(TreeNode treeNode, Integer preSum, int targetSum, int[] flag) {
         // flag[0] == 1 或当前递归到的节点为空，直接跳出方法
         if (flag[0] == 1 || treeNode == null) {
             return;
         }
-        preSum += treeNode.val;
+        preSum += treeNode.getVal();
         // 左递归路径值
-        recurve(treeNode.left, preSum, targetSum, flag);
+        recurve(treeNode.getLeft(), preSum, targetSum, flag);
         // 右递归路径值
-        recurve(treeNode.right, preSum, targetSum, flag);
+        recurve(treeNode.getRight(), preSum, targetSum, flag);
         // 判断是否找到等于目标和的路径 (叶子节点)
-        if (treeNode.left == null && treeNode.right == null) {
+        if (treeNode.getLeft() == null && treeNode.getRight() == null) {
             if (preSum == targetSum) {
                 flag[0] = 1;
             }
         }
     }
-
-
-
-
-
-
-
-
-    /**
-     * 定义二叉树 （题目给出条件）<br/>
-     * 提示：<br/>
-     * <br/>
-     * 树中节点数目在范围 [0, 100] 内<br/>
-     * -100 <= Node.val <= 100
-     */
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {
-        }
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val,TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
 }
