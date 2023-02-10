de77428c9938e892f8b0adec38f6b66d4214d568
Sally <1564358705@qq.com>
2023-02-10 16:55:57 +0800

    Q226InvertBinaryTreeTest

--- src/test/java/com/lonton/leetcode/easy/Q226InvertBinaryTreeTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q226InvertBinaryTreeTest.java
@@ -1,16 +1,18 @@
 package com.lonton.leetcode.easy;
 
-import com.lonton.leetcode.easy.Q226InvertBinaryTree;
-import org.junit.Test;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static com.lonton.leetcode.easy.Q226InvertBinaryTree.invertTree;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
  * 测试翻转二叉树 <p/>
- * 输入：root = [2,1,3] <p/>
- * 输出：[2,3,1]
+ * 将数组 [4,2,7,1,3,6,9] 构建成树(满子节点),并判断翻转后二叉树节点是否符合预期
+ * 将数组 [4,2,7,1] 构建成树(半子节点),并判断翻转后二叉树节点是否符合预期
+ *
  * @author 张利红
  */
 public class Q226InvertBinaryTreeTest {
@@ -19,16 +21,27 @@ public class Q226InvertBinaryTreeTest {
     public void test() {
 
         // 构建二叉树
-        Q226InvertBinaryTree.TreeNode root = new Q226InvertBinaryTree.TreeNode(2);
-        root.left = new Q226InvertBinaryTree.TreeNode(1);
-        root.right = new Q226InvertBinaryTree.TreeNode(3);
-        invertTree(root);
+        Integer[] nums = new Integer[]{4, 2, 7, 1, 3, 6, 9};
+        Integer[] nums2 = new Integer[]{4, 2, 7, 1};
+        TreeNode root1 = BinaryTree.arrayToTree(nums);
+        TreeNode root2 = BinaryTree.arrayToTree(nums2);
+
+        // 翻转树
+        invertTree(root1);
+        invertTree(root2);
 
         // 断言
-        assertEquals(1, root.right.val,"NotEquals");
-        assertEquals(2, root.val,"NotEquals");
-        assertEquals(3, root.left.val,"NotEquals");
+        Assertions.assertEquals(2, root1.getRight().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(1, root1.getRight().getRight().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(3, root1.getRight().getLeft().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(4, root1.getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(7, root1.getLeft().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(9, root1.getLeft().getLeft().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(6, root1.getLeft().getRight().getVal(), "NotEquals---翻转后树的节点与预期不符。");
 
-
+        Assertions.assertEquals(2, root2.getRight().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(1, root2.getRight().getRight().getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(4, root2.getVal(), "NotEquals---翻转后树的节点与预期不符。");
+        Assertions.assertEquals(7, root2.getLeft().getVal(), "NotEquals---翻转后树的节点与预期不符。");
     }
 }
