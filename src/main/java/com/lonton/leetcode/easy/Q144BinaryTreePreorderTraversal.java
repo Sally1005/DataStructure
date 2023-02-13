e5e4a26e620d41e1230697c181d4d99041ab4b16
Sally <1564358705@qq.com>
2023-02-13 15:10:35 +0800

    Q144BinaryTreePreorderTraversal

--- src/main/java/com/lonton/leetcode/easy/Q144BinaryTreePreorderTraversal.java
+++ src/main/java/com/lonton/leetcode/easy/Q144BinaryTreePreorderTraversal.java
@@ -1,70 +1,54 @@
 package com.lonton.leetcode.easy;
 
 
+import com.lonton.leetcode.common.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 144. 二叉树的前序遍历
+ *
  * @author 张利红
  */
-public class Q144BinaryTreePreorderTraversal {
+public class Q144BinaryTreePreorderTraversal extends TreeNode {
+    /**
+     * 前序遍历
+     *
+     * @param root 根节点
+     * @return 返回存储对象
+     */
     public static List<Integer> preorderTraversal(TreeNode root) {
         // 定义list集合，存储返回对象
-        List<Integer> vals = new ArrayList<>();
-        if(root == null){
-            return vals;
+        List<Integer> val = new ArrayList<>();
+        if (root == null) {
+            return val;
         }
         // 递归遍历数据
-        preorder(root, vals);
-        return vals;
+        preorder(root, val);
+        return val;
     }
-
 
     /**
      * 递归遍历下一个节点 <br/>
      * 根->左->右 <br/>
+     *
      * @param root 根节点 <br/>
-     * @param vals list集合容器
+     * @param val  list集合容器
      */
-    public static void preorder(TreeNode root, List<Integer> vals) {
-        if(root == null){
+    public static void preorder(TreeNode root, List<Integer> val) {
+        if (root == null) {
             return;
         }
-         // 判断根节点存在
-        vals.add(root.val);
-         // 判断是否有左节点
-        if (root.left != null) {
-            preorder(root.left, vals);
+        // 判断根节点存在
+        val.add(root.getVal());
+        // 判断是否有左节点
+        if (root.getLeft() != null) {
+            preorder(root.getLeft(), val);
         }
-         // 判断是否有右节点
-        if (root.right != null) {
-            preorder(root.right, vals);
-        }
-    }
-
-
-    /**
-     * 定义二叉树 （题目给出条件）<br/>
-     * 提示：<br/>
-     * <br/>
-     * 树中节点数目在范围 [0, 100] 内<br/>
-     * -100 <= Node.val <= 100
-     */
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {
-        }
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left,TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+        // 判断是否有右节点
+        if (root.getRight() != null) {
+            preorder(root.getRight(), val);
         }
     }
 }
