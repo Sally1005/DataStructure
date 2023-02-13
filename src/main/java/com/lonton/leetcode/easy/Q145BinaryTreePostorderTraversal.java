3be1d81932d3fc167061993dc8ea6fb7efea2ce6
Sally <1564358705@qq.com>
2023-02-13 15:10:57 +0800

    Q145BinaryTreePostorderTraversal

--- src/main/java/com/lonton/leetcode/easy/Q145BinaryTreePostorderTraversal.java
+++ src/main/java/com/lonton/leetcode/easy/Q145BinaryTreePostorderTraversal.java
@@ -1,68 +1,53 @@
 package com.lonton.leetcode.easy;
+
+import com.lonton.leetcode.common.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 145. 二叉树的后序遍历
+ *
  * @author 张利红
  */
-public class Q145BinaryTreePostorderTraversal {
+public class Q145BinaryTreePostorderTraversal extends TreeNode {
+    /**
+     * 后序遍历
+     *
+     * @param root 根节点
+     * @return 返回存储对象
+     */
     public static List<Integer> postorderTraversal(TreeNode root) {
         // 定义list集合，存储返回对象
-        List<Integer> vals = new ArrayList<>();
-        if(root == null){
-            return vals;
+        List<Integer> val = new ArrayList<>();
+        if (root == null) {
+            return val;
         }
         // 递归遍历数据
-        postorder(root, vals);
-        return vals;
+        postorder(root, val);
+        return val;
     }
 
     /**
      * 递归遍历下一个节点 <br/>
      * 左->右->根 <br/>
+     *
      * @param root 根节点 <br/>
-     * @param vals list集合容器
+     * @param val  list集合容器
      */
-    public static void postorder(TreeNode root, List<Integer> vals) {
-        if(root == null){
+    public static void postorder(TreeNode root, List<Integer> val) {
+        if (root == null) {
             return;
         }
         // 判断是否有左节点
-        if (root.left != null) {
-            postorder(root.left, vals);
+        if (root.getLeft() != null) {
+            postorder(root.getLeft(), val);
         }
         // 判断是否有右节点
-        if (root.right != null) {
-            postorder(root.right, vals);
+        if (root.getRight() != null) {
+            postorder(root.getRight(), val);
         }
         // 判断根节点存在
-        vals.add(root.val);
+        val.add(root.getVal());
     }
-    /**
-     * 定义二叉树 （题目给出条件）<br/>
-     * 提示：<br/>
-     * <br/>
-     * 树中节点数目在范围 [0, 100] 内<br/>
-     * -100 <= Node.val <= 100
-     */
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {
-        }
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left,TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-
-
 }
