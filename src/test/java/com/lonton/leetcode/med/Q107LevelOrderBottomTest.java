be91f624b506938290b981b55d3bf072fdf7ae54
Sally <1564358705@qq.com>
2023-02-13 15:26:03 +0800

    Q107LevelOrderBottomTest

--- src/test/java/com/lonton/leetcode/med/Q107LevelOrderBottomTest.java
+++ src/test/java/com/lonton/leetcode/med/Q107LevelOrderBottomTest.java
@@ -1,36 +1,32 @@
 package com.lonton.leetcode.med;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import com.lonton.leetcode.common.BinaryTree;
 
-import java.util.*;
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 测试二叉树的层级遍历 II <p/>
- * 输入：root = [3,9,20,null,null,15,7] <p/>
- * 输出：[[15,7],[9,20],[3]] <p/>
+ * 将数组 nums = [5,4,8,11,null,13,4,7,2,null,null,5,1],构建成二叉树,并判断输出的值是否为预期的[[15,7],[9,20],[3]]。
+ *
  * @author 张利红
  */
-@Slf4j
 public class Q107LevelOrderBottomTest {
-
-
     @Test
     public void test() {
         // 构建二叉树
-        Q107LevelOrderBottom.TreeNode treeNode = new Q107LevelOrderBottom.TreeNode(3,
-                new Q107LevelOrderBottom.TreeNode(9),
-                new Q107LevelOrderBottom.TreeNode(20, new Q107LevelOrderBottom.TreeNode(15), new Q107LevelOrderBottom.TreeNode(7))
-        );
-        List<List<Integer>> actual = new Q107LevelOrderBottom().levelOrderBottom(treeNode);
-        log.info("actual:{}",actual);
+        Integer[] nums = new Integer[]{3, 9, 20, null, null, 15, 7};
+        TreeNode node = BinaryTree.arrayToTree(nums);
 
         List<List<Integer>> expected = new ArrayList<>(
                 Arrays.asList(Arrays.asList(15, 7), Arrays.asList(9, 20), Arrays.asList(3)));
-        log.info("expected:{}",expected);
 
-        // 断言
-        Assertions.assertEquals(expected,actual,"NotEquals");
+        Assertions.assertEquals(expected, new Q107LevelOrderBottom().levelOrderBottom(node),
+                "NotEquals---二叉树的层级遍历II实际得到的结果与预期结果不相等。");
     }
 }
