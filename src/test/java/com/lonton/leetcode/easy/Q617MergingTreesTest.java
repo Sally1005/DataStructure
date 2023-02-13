6115afe253f092e77826ef8574f8b1e6290eeef7
Sally <1564358705@qq.com>
2023-02-13 15:25:04 +0800

    Q617MergingTreesTest

--- src/test/java/com/lonton/leetcode/easy/Q617MergingTreesTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q617MergingTreesTest.java
@@ -1,24 +1,39 @@
 package com.lonton.leetcode.easy;
 
 
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Arrays;
+
 /**
  * 617. 测试合并二叉树
- * 输入：root1 = [1], root2 = [1,2]<p/>
- * 输出：[2,2] <p/>
+ * 将数组 nums1 = [1]，nums2 = [1,2] 构建成二叉树，并判断输出的值是否为预期的[2，2]。
+ *
  * @author 张利红
  */
 public class Q617MergingTreesTest {
-
     @Test
-    public  void  test(){
+    public void test() {
         // 构建二叉树
-        Q617MergingTrees.TreeNode treeNode1 = new Q617MergingTrees.TreeNode(1);
-        Q617MergingTrees.TreeNode treeNode2 = new Q617MergingTrees.TreeNode(1, new Q617MergingTrees.TreeNode(2), null);
+        Integer[] nums1 = new Integer[]{1};
+        TreeNode root1 = BinaryTree.arrayToTree(nums1);
+        Integer[] nums2 = new Integer[]{1, 2};
+        TreeNode root2 = BinaryTree.arrayToTree(nums2);
 
-       Assertions.assertEquals(2,Q617MergingTrees.mergeTrees(treeNode1,treeNode2).val,"合并二叉树失败");
+        TreeNode node = Q617MergingTrees.mergeTrees(root1, root2);
+        Integer[] array = BinaryTree.treeToArray(node);
+        // 因为数组后面的null不影响数据，为了方便测试，将数组后面的null值都去掉
+        int idx = array.length - 1;
+        while (array[idx] == null) {
+            idx--;
+        }
+        Integer[] nullFilteredArr = Arrays.copyOf(array, idx + 1);
+
+        Assertions.assertEquals(Arrays.asList(2, 2), Arrays.asList(nullFilteredArr),
+                "NotEquals---合并二叉树失败，实际得到的结果与预期结果不相等。");
     }
 
 }
