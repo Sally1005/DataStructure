004642592e33400eb27e41cf6ed1c8af707381d3
Sally <1564358705@qq.com>
2023-02-13 15:33:20 +0800

    Q94BinaryTreeInOrderTraversal

---
+++ src/main/java/com/lonton/leetcode/easy/Q94BinaryTreeInOrderTraversal.java
@@ -0,0 +1,50 @@
+package com.lonton.leetcode.easy;
+
+import com.lonton.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历 <p/>
+ *
+ * @author 张利红
+ */
+public class Q94BinaryTreeInOrderTraversal extends TreeNode {
+    /**
+     * 中序遍历
+     *
+     * @param root 根节点
+     * @return 返回存储对象
+     */
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        // 定义list集合，存储返回对象
+        List<Integer> val = new ArrayList<>();
+        if (root == null) {
+            return val;
+        }
+        // 递归遍历数据
+        inorder(root, val);
+        return val;
+    }
+
+    /**
+     * 递归遍历下一个节点 （左->根->右）
+     *
+     * @param root 根节点 <br/>
+     * @param val  list集合容器
+     */
+    public static void inorder(TreeNode root, List<Integer> val) {
+        // 判断是否有左节点
+        if (root.getLeft() != null) {
+            inorder(root.getLeft(), val);
+        }
+        // 判断当前左节点是否遍历完或者说是左节点根本不存在
+        val.add(root.getVal());
+        // 判断是否有右节点
+        if (root.getRight() != null) {
+            inorder(root.getRight(), val);
+        }
+    }
+
+}
