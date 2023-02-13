55b66467c07bb6be8afbd79effd076dffe4620f8
Sally <1564358705@qq.com>
2023-02-13 15:16:22 +0800

    Q104MaxDepthTest

--- src/test/java/com/lonton/leetcode/easy/Q104MaxDepthTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q104MaxDepthTest.java
@@ -1,34 +1,24 @@
 package com.lonton.leetcode.easy;
 
-import com.lonton.leetcode.easy.Q104MaxDepth;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
- * 给定二叉树 [3,9,20,null,null,15,7],校验二叉树最大深度是否正确，如检验该树的最大深度是否为3。<p/>
- *            3     <p/>
- *          /  \    <p/>
- *        9     20  <p/>
- *             /  \ <p/>
- *           15   7 <p/>
- *  @author 张利红
+ * 将数组 nums = [3,9,20,null,null,15,7] 构建成二叉树,并校验二叉树最大深度是否为预期值3。<p/>
+ *
+ * @author 张利红
  */
-@Slf4j
 public class Q104MaxDepthTest {
+    @Test
+    public void test() {
+        // 构建二叉树
+        Integer[] nums = new Integer[]{3, 9, 20, null, null, 15, 7};
+        TreeNode root = BinaryTree.arrayToTree(nums);
 
-     @Test
-     public void test(){
-           // 建树
-          Q104MaxDepth.TreeNode tree = new Q104MaxDepth.TreeNode(3,new Q104MaxDepth.TreeNode(9),
-                  new Q104MaxDepth.TreeNode(20,
-                          new Q104MaxDepth.TreeNode(15),
-                          new Q104MaxDepth.TreeNode(7)));
-           // 断言判断证明没错
-          Assertions.assertSame(3,new Q104MaxDepth().maxDepth(tree),"最大深度计算错误。");
-
-     }
-
-
-
-
+        Assertions.assertEquals(3, new Q104MaxDepth().maxDepth(root),
+                "NotEquals---最大深度计算错误错误，实际计算值与预期不符。");
+    }
 }
