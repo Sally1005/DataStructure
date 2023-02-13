fe8380c81c3623a2e52f0f34c347c3cb656b8a2b
Sally <1564358705@qq.com>
2023-02-13 15:24:46 +0800

    Q404SumOfLeftLeavesTest

--- src/test/java/com/lonton/leetcode/easy/Q404SumOfLeftLeavesTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q404SumOfLeftLeavesTest.java
@@ -1,29 +1,25 @@
 package com.lonton.leetcode.easy;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-import org.junit.Test;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * 测试左叶子之和 <p/>
- * 输入: root = [3,9,20,null,null,15,7] <p/>
- * 输出: 24 <p/>
- * 解释: 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24 <p/>
+ * 将数组 nums = [3,9,20,null,null,15,7]构建成二叉树，并判断输出的值是否为预期的24。 <p/>
  *
+ * @author 张利红
  */
-@Slf4j
 public class Q404SumOfLeftLeavesTest {
     @Test
-    public void test(){
-        // 建树
-        Q404SumOfLeftLeaves.TreeNode tree = new Q404SumOfLeftLeaves.TreeNode(3,new Q404SumOfLeftLeaves.TreeNode(9),
-                new Q404SumOfLeftLeaves.TreeNode(20,
-                        new Q404SumOfLeftLeaves.TreeNode(15),
-                        new Q404SumOfLeftLeaves.TreeNode(7)));
+    public void test() {
+        // 构建二叉树
+        Integer[] nums = new Integer[]{3, 9, 20, null, null, 15, 7};
+        TreeNode root = BinaryTree.arrayToTree(nums);
 
-        // 断言
-        Assert.assertEquals("左叶子之和计算错误",24, new Q404SumOfLeftLeaves().sumOfLeftLeaves(tree));
+        Assertions.assertEquals(24, new Q404SumOfLeftLeaves().sumOfLeftLeaves(root),
+                "NotEquals---左叶子之和计算错误，实际结果与预期值不相等。");
     }
-
 
 }
