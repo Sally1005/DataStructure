217129bf740c49d5e97a9142233b38bb81d534cf
Sally <1564358705@qq.com>
2023-02-13 15:24:29 +0800

    Q257AllPathsToBinaryTreesTest

--- src/test/java/com/lonton/leetcode/easy/Q257AllPathsToBinaryTreesTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q257AllPathsToBinaryTreesTest.java
@@ -1,35 +1,33 @@
 package com.lonton.leetcode.easy;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.util.List;
-
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * 测试二叉树的所有路径 <p/>
- * 输入：root = [1,2,3,null,5] <p/>
- * 输出：["1->2->5","1->3"] <p/>
+ * 将数组 [1,2,3,null,5] 构建成二叉树,
+ * 并判断该树中从 根节点到叶子节点 的路径 是否为预期条数 (2), 并检查结果集每一条路径是否计算正确。 <p/>
+ *
  * @author 张利红
  */
-@Slf4j
 public class Q257AllPathsToBinaryTreesTest {
     @Test
-    public void test(){
+    public void test() {
         // 构建二叉树
-        Q257AllPathsToBinaryTrees.TreeNode root = new Q257AllPathsToBinaryTrees.TreeNode(1);
-        Q257AllPathsToBinaryTrees.TreeNode treeNode2 = new Q257AllPathsToBinaryTrees.TreeNode(2);
-        Q257AllPathsToBinaryTrees.TreeNode treeNode3 = new Q257AllPathsToBinaryTrees.TreeNode(3);
-        Q257AllPathsToBinaryTrees.TreeNode treeNode4 = new Q257AllPathsToBinaryTrees.TreeNode(5);
-        root.left = treeNode2;
-        root.right =treeNode3;
-        treeNode2.right = treeNode4;
+        Integer[] nums = new Integer[]{1, 2, 3, null, 5};
+        TreeNode root = BinaryTree.arrayToTree(nums);
+
         // 断言
-        List<String> res = Q257AllPathsToBinaryTrees.binaryTreePaths(root);
-        log.info("res={}",res);
-        Assert.assertEquals("NotEquals",2,res.size());
+        Assertions.assertEquals(2,
+                Q257AllPathsToBinaryTrees.binaryTreePaths(root).size(),
+                "NotEquals---从根节点到叶子节点的路径条数与预期结果不相等。");
+        Assertions.assertTrue(
+                Q257AllPathsToBinaryTrees.binaryTreePaths(root).contains("1->3"),
+                "NotTrue---结果集路径计算错误，实际得到的路径不符合预期。");
+        Assertions.assertTrue(
+                Q257AllPathsToBinaryTrees.binaryTreePaths(root).contains("1->2->5"),
+                "NotTrue---结果集路径计算错误，实际得到的路径不符合预期。");
     }
-
-
 }
