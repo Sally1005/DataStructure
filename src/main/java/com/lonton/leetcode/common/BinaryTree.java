558cb259d356f9adaeed9fc850dbfa5e09787920
Sally <1564358705@qq.com>
2023-02-13 11:02:10 +0800

    BinaryTree

--- src/main/java/com/lonton/leetcode/common/BinaryTree.java
+++ src/main/java/com/lonton/leetcode/common/BinaryTree.java
@@ -1,10 +1,10 @@
 package com.lonton.leetcode.common;
 
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
 
 /**
  * 二叉树类
@@ -26,21 +26,16 @@ public class BinaryTree {
     public static TreeNode arrayToTree(Integer[] array) {
         // 构建一棵二叉树
         BinaryTree tree = new BinaryTree();
-
         // 如果数组为空，则返回空数组
         if (array.length == 0) return null;
-
         // 数组第一个为根节点
         tree.root = new TreeNode(array[0]);
-
         // 初始化树的下标
         int treeIdx = 0;
         int dataIdx = 0;
-
         // 创建一个集合存放数组
         ArrayList<TreeNode> treeNodes = new ArrayList<>();
         treeNodes.add(tree.root);
-
         // 循环遍历数组
         for (dataIdx = 1; dataIdx < array.length; dataIdx += 2) {
             // 放置左节点
@@ -61,6 +56,38 @@ public class BinaryTree {
         }
         return tree.root;
 
+    }
+
+    /**
+     * 树转为数组
+     *
+     * @param root 根节点
+     * @return 数组
+     */
+    public static Integer[] treeToArray(TreeNode root) {
+        // 根节点为空，返回一个空数组
+        if (root == null) return new Integer[0];
+        // 由于不知道数组长度，先将元素存入list中
+        List<Integer> list = new ArrayList<>();
+        // 创建队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 加入根节点
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node != null)
+                    list.add(node.val);
+                else
+                    list.add(null);
+                if (node == null || (queue.isEmpty() && node.left == null && node.right == null))
+                    continue;
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return list.toArray(new Integer[0]);
     }
 
 }
