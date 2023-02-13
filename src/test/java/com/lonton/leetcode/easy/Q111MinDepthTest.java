996a6decbaa3ee6ae6979ae321effb7cba7cf0c5
Sally <1564358705@qq.com>
2023-02-13 15:18:11 +0800

    Q111MinDepthTest

--- src/test/java/com/lonton/leetcode/easy/Q111MinDepthTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q111MinDepthTest.java
@@ -1,72 +1,40 @@
 package com.lonton.leetcode.easy;
 
-import com.lonton.leetcode.easy.Q111MinDepth;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * 示例1~~输入：root = [3,9,20,null,null,15,7],校验二叉树最小深度是否正确，如检验该树的最小深度是否为 2。<p/>
- *       输出：2 <p/>
- *      3       <p/>
- *     / \      <p/>
- *    9   20    <p/>
- *       /  \   <p/>
- *      15   7  <p/>
- *  <p/>
- * 示例2~~输入：root = [2,null,3,null,4,null,5,null,6],校验二叉树最小深度是否正确，如检验该树的最小深度是否为 5。<p/>
- *       输出：5  <p/>
- *      2        <p/>
- *       \       <p/>
- *        3      <p/>
- *         \     <p/>
- *          4    <p/>
- *           \   <p/>
- *            5  <p/>
- *             \ <p/>
- *              6<p/>
- * 示例3~~输入：root = [2,null,3,null,4,null,5,null,6],校验二叉树最小深度是否正确，如检验该树的最小深度是否为 5。<p/>
- *       输出：5  <p/>
- *      2        <p/>
- *       \       <p/>
- *        3      <p/>
- *         \     <p/>
- *          4    <p/>
- *         /  \  <p/>
- *         1   5 <p/>
- *             \ <p/>
- *              7<p/>
+ * 示例1：将数组 nums1 = [3,9,20,null,null,15,7] 构建为二叉树，并校验二叉树最小深度是否为 2。
+ * <p>
+ * 示例2：将数组 nums2 = [2,null,3,null,4,null,5,null,6]构建为二叉树,并校验二叉树最小深度是否为 5。
+ * <p>
+ * 示例3：将数组 nums3 = [2,null,3,null,4,1,5,null,7]构建为二叉树,并校验二叉树最小深度是否为 4。
+ *
  * @author 张利红
  */
 public class Q111MinDepthTest {
     @Test
-    public  void  test(){
-         // 建树
-         Q111MinDepth.TreeNode tree = new Q111MinDepth.TreeNode(3,
-                new Q111MinDepth.TreeNode(9),
-                new Q111MinDepth.TreeNode(20,
-                        new Q111MinDepth.TreeNode(15),new Q111MinDepth.TreeNode(7)));
+    public void test() {
+        // 构建二叉树
+        Integer[] nums1 = new Integer[]{3, 9, 20, null, null, 15, 7};
+        TreeNode tree1 = BinaryTree.arrayToTree(nums1);
 
-        Q111MinDepth.TreeNode tree2 = new Q111MinDepth.TreeNode(2,null,
-                new Q111MinDepth.TreeNode(3,null,
-                        new Q111MinDepth.TreeNode(4,null,
-                                new Q111MinDepth.TreeNode(5,null,
-                                        new Q111MinDepth.TreeNode(6))))
-                );
-        Q111MinDepth.TreeNode tree3 = new Q111MinDepth.TreeNode(2,null,
-                new Q111MinDepth.TreeNode(3,null,
-                        new Q111MinDepth.TreeNode(4,new Q111MinDepth.TreeNode(1),
-                                new Q111MinDepth.TreeNode(5,null,
-                                        new Q111MinDepth.TreeNode(7))))
-        );
+        Assertions.assertEquals(2, new Q111MinDepth().minDepth(tree1),
+                "NotEquals---最小深度计算错误，实际计算值与预期不符。");
 
-        // 断言判断证明没错
-        Assertions.assertSame(2,new Q111MinDepth().minDepth(tree),"最小深度计算错误");
+        Integer[] nums2 = new Integer[]{2, null, 3, null, 4, null, 5, null, 6};
+        TreeNode tree2 = BinaryTree.arrayToTree(nums2);
 
-        // 断言判断证明没错
-        Assertions.assertSame(5,new Q111MinDepth().minDepth(tree2),"最小深度计算错误");
+        Assertions.assertEquals(5, new Q111MinDepth().minDepth(tree2),
+                "NotEquals---最小深度计算错误，实际计算值与预期不符。");
 
-        // 断言判断证明没错
-        Assertions.assertSame(4,new Q111MinDepth().minDepth(tree3),"最小深度计算错误");
+        Integer[] nums3 = new Integer[]{2, null, 3, null, 4, 1, 5, null, 7};
+        TreeNode tree3 = BinaryTree.arrayToTree(nums3);
+
+        Assertions.assertEquals(4, new Q111MinDepth().minDepth(tree3),
+                "NotEquals---最小深度计算错误，实际计算值与预期不符。");
     }
 
 }
