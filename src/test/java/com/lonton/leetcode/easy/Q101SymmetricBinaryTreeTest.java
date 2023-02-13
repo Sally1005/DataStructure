750d066c241d2bbde013c215ccb0d034660d6ae3
Sally <1564358705@qq.com>
2023-02-13 15:15:57 +0800

    Q101SymmetricBinaryTreeTest

--- src/test/java/com/lonton/leetcode/easy/Q101SymmetricBinaryTreeTest.java
+++ src/test/java/com/lonton/leetcode/easy/Q101SymmetricBinaryTreeTest.java
@@ -1,27 +1,26 @@
 package com.lonton.leetcode.easy;
 
-import com.lonton.leetcode.easy.Q101SymmetricBinaryTree;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import com.lonton.leetcode.common.BinaryTree;
+import com.lonton.leetcode.common.TreeNode;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.lonton.leetcode.easy.Q101SymmetricBinaryTree.isSymmetric;
+
 /**
  * 测试二叉树是否为对称的树 <p/>
- * 输入：root = [1,2,2,3,4,4,3] <p/>
- * 输出：true <p/>
+ * 将数组 nums = [1,2,2,3,4,4,3] 构建成二叉树，并检验 二叉树是否为对称的树，预期值为true。<p/>
+ *
  * @author 张利红
  */
-@Slf4j
 public class Q101SymmetricBinaryTreeTest {
     @Test
     public void test() {
         // 构建二叉树
-        Q101SymmetricBinaryTree.TreeNode treeNode = new Q101SymmetricBinaryTree.TreeNode(1,
-               new Q101SymmetricBinaryTree.TreeNode(2, new Q101SymmetricBinaryTree.TreeNode(3), new Q101SymmetricBinaryTree.TreeNode(4)),
-                new Q101SymmetricBinaryTree.TreeNode(2, new Q101SymmetricBinaryTree.TreeNode(4), new Q101SymmetricBinaryTree.TreeNode(3))
-        );
+        Integer[] nums = new Integer[]{1, 2, 2, 3, 4, 4, 3};
+        TreeNode root = BinaryTree.arrayToTree(nums);
 
-        // 断言判断证明没错
-        Assertions.assertSame(true,
-                Q101SymmetricBinaryTree.isSymmetric(treeNode),"treeNode不是镜像树");
+        Assertions.assertTrue(isSymmetric(root),
+                "False---实际测试中的树结构不是镜像树。");
     }
 }
