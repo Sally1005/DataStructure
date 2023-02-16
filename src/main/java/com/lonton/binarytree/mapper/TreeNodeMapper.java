93b5a0aaef406bed11315c812b5fab2997386760
Sally <1564358705@qq.com>
2023-02-16 11:40:58 +0800

    TreeNodeMapper

--- src/main/java/com/lonton/binarytree/mapper/TreeNodeMapper.java
+++ src/main/java/com/lonton/binarytree/mapper/TreeNodeMapper.java
@@ -4,17 +4,19 @@ import com.lonton.binarytree.pojo.BinaryTree;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 /**
  * TreeNode的mapper接口
  * <p/>
+ *
  * @author 张利红
  */
 @Repository // 解决Mapper注入爆红问题
-public interface TreeNodeMapper{
+public interface TreeNodeMapper {
     /**
      * 查询节点列表
-     * <br>
+     *
      * @return 节点列表
      */
     List<BinaryTree.TreeNode> list();
-    }
+}
