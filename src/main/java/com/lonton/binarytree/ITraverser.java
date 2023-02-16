5ca890ff55fc8dd3dab02028d1aaa2f84c09cb4e
Sally <1564358705@qq.com>
2023-02-16 11:39:42 +0800

    删除重复的doVisitor方法

--- src/main/java/com/lonton/binarytree/ITraverser.java
+++ src/main/java/com/lonton/binarytree/ITraverser.java
@@ -5,26 +5,18 @@ import com.lonton.binarytree.pojo.BinaryTree;
 /**
  * 遍历接口
  * <p/>
+ *
  * @author 张利红
  */
 public interface ITraverser {
-    /**
-     * 遍历树 (在具体的实现类中实现）
-     * @param tree 二叉树
-     * @param visitor 访问visitor
-     * @param param 可变参数
-     * @return 任意类型
-     */
-    Object traverse(BinaryTree tree, IVisitor visitor,Object... param);
 
     /**
      * 访问树 (在抽象类中实现）
+     *
      * @param binaryTree 二叉树
-     * @param visitor 访问visitor
-     * @param param 可变参数
+     * @param visitor    访问visitor
+     * @param param      可变参数
      * @return 任意类型
      */
-    default Object doVisitor(BinaryTree binaryTree, IVisitor visitor, Object... param){
-        return traverse(binaryTree, visitor, param);
-    }
+    BinaryTree.TreeNode traverse(BinaryTree binaryTree, IVisitor visitor, Object... param);
 }
