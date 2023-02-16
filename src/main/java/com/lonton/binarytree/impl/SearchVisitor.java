548c7e7c02b4e255ac8797524ce6d7595c32d7bb
Sally <1564358705@qq.com>
2023-02-16 11:45:12 +0800

    重载visit方法

--- src/main/java/com/lonton/binarytree/impl/SearchVisitor.java
+++ src/main/java/com/lonton/binarytree/impl/SearchVisitor.java
@@ -1,7 +1,7 @@
 package com.lonton.binarytree.impl;
 
-import com.lonton.binarytree.pojo.BinaryTree;
 import com.lonton.binarytree.IVisitor;
+import com.lonton.binarytree.pojo.BinaryTree;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -11,6 +11,7 @@ import java.util.function.Predicate;
 /**
  * 根据id查找节点
  * <p/>
+ *
  * @author 张利红
  */
 @Data
@@ -31,34 +32,18 @@ public class SearchVisitor implements IVisitor {
      */
     private List<BinaryTree.TreeNode> filterNode = new ArrayList<>();
 
-
     /**
      * 判断符合条件参数
      */
-    private Predicate<BinaryTree.TreeNode> filter;
+    private List<Predicate<BinaryTree.TreeNode>> filters;
 
-    /**
-     * 根据id查找节点<重载>
-     * @param node 节点
-     * @param predicate 预测(过滤输入内容是否符合条件)
-     * @return 所找节点
-     */
-    @Override
-    public Object visit(BinaryTree.TreeNode node, Predicate<BinaryTree.TreeNode> predicate) {
-        // 若节点id值刚好的等于所查询的id，则返回当前节点
-        if (node.getId() == id ) {
-            this.foundNode = node;
-        }
-        // 若为true,则将节点信息加入符合条件的数据集合中
-        if (predicate.test(node)){
-            filterNode.add(node);
-        }
-        return node;
-    }
+    // private Predicate<BinaryTree.TreeNode> filter;
+
 
     /**
      * 根据id查找节点
-     * @param node 节点
+     *
+     * @param node  节点
      * @param param 可变参数
      * @return 所找节点
      */
@@ -71,9 +56,33 @@ public class SearchVisitor implements IVisitor {
         return node;
     }
 
+    /**
+     * 根据id查找节点 <重载>
+     *
+     * @param node      节点
+     * @param predicate 过滤条件
+     * @return 所找节点
+     */
+    @Override
+    public Object visit(BinaryTree.TreeNode node, List<Predicate<BinaryTree.TreeNode>> predicate) {
+        // 若节点id值刚好的等于所查询的id，则返回当前节点
+        if (node.getId() == id) {
+            this.foundNode = node;
+        }
+        // 若为true,则将节点信息加入符合条件的数据集合中
+        if (predicate != null) {
+            for (Predicate<BinaryTree.TreeNode> treeNodePredicate : predicate) {
+                if (treeNodePredicate.test(node)) {
+                    filterNode.add(node);
+                }
+            }
+        }
+        return node;
+    }
 
     /**
      * 设置 id
+     *
      * @param id id
      */
     public void setId(int id) {
@@ -81,7 +90,17 @@ public class SearchVisitor implements IVisitor {
     }
 
     /**
+     * 节点过滤
+     *
+     * @return 过滤后的节点
+     */
+    public List<BinaryTree.TreeNode> getFilterNode() {
+        return filterNode;
+    }
+
+    /**
      * 获取已找到的节点
+     *
      * @return 已找到的节点
      */
     @Override
@@ -89,16 +108,17 @@ public class SearchVisitor implements IVisitor {
         return foundNode;
     }
 
-
-    public List<BinaryTree.TreeNode> getFilterNode() {
-        return filterNode;
-    }
-
-    /**
-     * 如果条件为空，则无过滤条件
-     */
     @Override
-    public Predicate<BinaryTree.TreeNode> predicate() {
-        return filter == null?(s-> true) :(filter);
+    public List<Predicate<BinaryTree.TreeNode>> predicates() {
+        return this.filters;
     }
+
+
+//    /**
+//     * 如果条件为空，则无过滤条件
+//     */
+//    @Override
+//    public Predicate<BinaryTree.TreeNode> predicate() {
+//        return filter == null?(s-> true) :(filter);
+//    }
 }
