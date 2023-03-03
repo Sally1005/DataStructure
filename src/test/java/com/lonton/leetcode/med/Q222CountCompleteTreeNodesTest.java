a26e2dec992801131f30a97926e5877ab5402823
Sally <1564358705@qq.com>
2023-03-03 14:20:01 +0800

    Q222CountCompleteTreeNodesTest

---
+++ src/test/java/com/lonton/leetcode/med/Q222CountCompleteTreeNodesTest.java
@@ -0,0 +1,38 @@
+package com.lonton.leetcode.med;
+
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试求完全二叉树的节点个数  <p/>
+ * 示例1：将数组 [1,2,3,4,5,6],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 6；
+ * 示例2：将数组 [1,2,3,4,5,6,7],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 7；
+ * 示例3：将数组 [],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 0。
+ *
+ * @author 张利红
+ */
+public class Q222CountCompleteTreeNodesTest {
+
+    @Test
+    public void test() {
+        // 构建完全二叉树
+        Integer[] nums = new Integer[]{1, 2, 3, 4, 5, 6};
+        TreeNode root = TreeNode.arrayToTree(nums);
+
+        Integer[] nums2 = new Integer[]{1, 2, 3, 4, 5, 6, 7};
+        TreeNode root2 = TreeNode.arrayToTree(nums2);
+
+        Integer[] nums3 = new Integer[0];
+        TreeNode root3 = TreeNode.arrayToTree(nums3);
+
+        int result = new Q222CountCompleteTreeNodes().countNodes(root);
+        int result2 = new Q222CountCompleteTreeNodes().countNodes(root2);
+        int result3 = new Q222CountCompleteTreeNodes().countNodes(root3);
+
+        // 断言
+        Assertions.assertEquals(6, result, "完全二叉树的节点个数计算错误。");
+        Assertions.assertEquals(7, result2, "完全二叉树的节点个数计算错误。");
+        Assertions.assertEquals(0, result3, "完全二叉树的节点个数计算错误。");
+    }
+}
