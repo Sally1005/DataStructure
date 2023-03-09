757021e071f184db3b4c9ef70fd0a4b782fc8952
Sally <1564358705@qq.com>
2023-03-09 11:33:01 +0800

    增加为null时情况，并修改测试说明

--- src/test/java/com/lonton/leetcode/med/Q222CountCompleteTreeNodesTest.java
+++ src/test/java/com/lonton/leetcode/med/Q222CountCompleteTreeNodesTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 
 /**
  * 测试求完全二叉树的节点个数  <p/>
- * 示例1：将数组 [1,2,3,4,5,6],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 6；
- * 示例2：将数组 [1,2,3,4,5,6,7],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 7；
- * 示例3：将数组 [],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 0。
+ * 示例1(不满完全二叉树）：将数组 [1,2,3,4,5,6],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 6；
+ * 示例2(满完全二叉树）：将数组 [1,2,3,4,5,6,7],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 7；
+ * 示例3(空完全二叉树）：将数组 [],构建成一棵完全二叉树,并判断完全二叉树的节点个数为 0。
+ * 示例4(完全二叉树为null）：判断完全二叉树的节点个数为 0。
  *
  * @author 张利红
  */
@@ -23,7 +24,7 @@ public class Q222CountCompleteTreeNodesTest {
         int result = new Q222CountCompleteTreeNodes().countNodes(root);
 
         // 断言
-        Assertions.assertEquals(6, result, "完全二叉树的节点个数计算错误。");
+        Assertions.assertEquals(6, result, "当完全二叉树为不满完全二叉树时的节点个数计算错误。");
     }
 
     @Test
@@ -35,7 +36,7 @@ public class Q222CountCompleteTreeNodesTest {
         int result2 = new Q222CountCompleteTreeNodes().countNodes(root2);
 
         // 断言
-        Assertions.assertEquals(7, result2, "完全二叉树的节点个数计算错误。");
+        Assertions.assertEquals(7, result2, "当完全二叉树为满完全二叉树时的节点个数计算错误。");
 
     }
 
@@ -48,6 +49,15 @@ public class Q222CountCompleteTreeNodesTest {
         int result3 = new Q222CountCompleteTreeNodes().countNodes(root3);
 
         // 断言
-        Assertions.assertEquals(0, result3, "完全二叉树的节点个数计算错误。");
+        Assertions.assertEquals(0, result3, "当完全二叉树为空完全二叉树时的节点个数计算错误。");
+    }
+
+    @Test
+    public void test4() {
+
+        int result4 = new Q222CountCompleteTreeNodes().countNodes(null);
+
+        // 断言
+        Assertions.assertEquals(0, result4, "当完全二叉树为null时的节点个数计算错误。");
     }
 }
