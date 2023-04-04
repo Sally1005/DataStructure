9a9c9b1a4cf1f183bae12c386663b14063e84592
Sally <1564358705@qq.com>
2023-04-04 11:45:54 +0800

    AVisitor

--- src/main/java/com/lonton/binarytree/AVisitor.java
+++ src/main/java/com/lonton/binarytree/AVisitor.java
@@ -6,8 +6,10 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+
 /**
  * 访问visitor 抽象类
+ *
  * @author 张利红
  */
 
@@ -16,15 +18,39 @@ public abstract class AVisitor {
     /**
      * 过滤属性
      */
-    private Predicate filter;
+    private List<Predicate<BinaryTree.TreeNode>> filters = new ArrayList<>();
 
     /**
-     * 过滤(与关系)
-     *
-     * @return 满足过滤条件的数据
+     * 添加过滤器
      */
-    public List<Predicate<BinaryTree.TreeNode>> addFilter(){
-        return new ArrayList<>();
-
+    public void addFilter(Predicate<BinaryTree.TreeNode> filter) {
+        filters.add(filter);
     }
+
+    /**
+     * 访问树
+     *
+     * @param node   节点
+     * @param filter 过滤器
+     * @return 任意类型
+     */
+    public Object visit(BinaryTree.TreeNode node, Predicate<BinaryTree.TreeNode> filter) {
+        for (Predicate<BinaryTree.TreeNode> f : filters) {
+            // 逐一添加所有过滤器
+            filter = filter.and(f);
+        }
+        if (filter.test(node)) {
+            return doVisit(node);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 方便重载以便具体实现类返回相应的返回值
+     *
+     * @param node 节点
+     * @return 任意类型
+     */
+    protected abstract Object doVisit(BinaryTree.TreeNode node);
 }
