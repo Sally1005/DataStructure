72d2d03e2323ace9496fb523bcfd96f55ff30ae1
Sally <1564358705@qq.com>
2023-04-17 14:53:53 +0800

    Q450DeleteNodeInBST

---
+++ src/main/java/com/lonton/leetcode/med/Q450DeleteNodeInBST.java
@@ -0,0 +1,71 @@
+package com.lonton.leetcode.med;
+
+import com.lonton.leetcode.common.TreeNode;
+
+/**
+ * 450. 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，<p>
+ * 并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。<p>
+ * 思路：<p>
+ * <ol>
+ *     <li> 未找到目标节点或根节点为空；
+ *     <li> 找到目标节点。为叶子节点（左空 & 右空）；
+ *     <li> （左空 & 右非空）；
+ *     <li> （左非空 & 右空）；
+ *     <li> （左非空 & 右非空）。
+ * </ol>
+ *
+ * @author 张利红
+ */
+public class Q450DeleteNodeInBST {
+    /**
+     * @param root 根节点
+     * @param key  目标删除元素
+     * @return （更新后）根节点
+     */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        // 根节点为空或者遍历完树还未找到要删除的目标元素
+        if (root == null) {
+            return null;
+        }
+        // 递归地向左子树删除元素
+        if (key < (int) root.getVal()) {
+            root.setLeft(deleteNode(root.getLeft(), key));
+            // 递归地向右子树删除元素
+        } else if (key > (int) root.getVal()) {
+            root.setRight(deleteNode(root.getRight(), key));
+        } else {
+            // 找到要删除的节点 为叶子节点（左空 & 右空）
+            if (root.getLeft() == null && root.getRight() == null) {
+                return null;
+            }
+            // 节点只有一个
+            if (root.getLeft() == null) {
+                return root.getRight();
+            } else if (root.getRight() == null) {
+                return root.getLeft();
+            }
+
+            // 节点有两个子节点，找到右子树的最小节点
+            root.setVal(minValue(root.getRight()));
+
+            // 删除右子树的最小节点
+            root.setRight(deleteNode(root.getRight(), (int) root.getVal()));
+        }
+        return root;
+    }
+
+    /**
+     * 返回右子树下较小的值
+     *
+     * @param root 根节点
+     * @return 较小的节点值
+     */
+    int minValue(TreeNode root) {
+        int minv = (int) root.getVal();
+        while (root.getLeft() != null) {
+            minv = (int) root.getLeft().getVal();
+            root = root.getLeft();
+        }
+        return minv;
+    }
+}
---
+++ src/test/java/com/lonton/leetcode/med/Q450DeleteNodeInBSTTest.java
@@ -0,0 +1,65 @@
+package com.lonton.leetcode.med;
+
+import com.lonton.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 450. 删除二叉搜索树
+ * <ol>
+ *     <li> 未找到目标节点或根节点为空；给定数组为 root = [5,3,6,2,4,null,7], key = 0,判断是否符合预期[5,3,6,2,4,null,7]。
+ *     <li> 找到目标节点。为叶子节点（左空 & 右空）；给定数组为 root = [5,3,6,2,4,null,7], key = 2,判断是否符合预期[5,3,6,null,4,null,7]。
+ *     <li> （左空 & 右非空）；给定数组为 root = [5,3,6,2,4,null,7], key = 6,判断是否符合预期[5,3,7,2,4]。
+ *     <li> （左非空 & 右空）；给定数组为 root = [5,3,8,2,4,7], key = 8,判断是否符合预期[5,3,7,2,4]。
+ *     <li> （左非空 & 右非空）：给定数组为 root = [5,3,6,2,4,null,7], key = 3,判断是否符合预期[5,4,6,2,null,null,7]。
+ * </ol>
+ *
+ * @author 张利红
+ */
+public class Q450DeleteNodeInBSTTest {
+
+    @Test
+    public void testEmpty() {
+        TreeNode root = new TreeNode<>().arrayToTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        TreeNode tree = new Q450DeleteNodeInBST().deleteNode(root, 0);
+
+        Assertions.assertArrayEquals(new Integer[]{5, 3, 6, 2, 4, null, 7}, tree.treeToArray(tree, Integer.class),
+                "当未找到目标节点或根节点为空时，删除二叉搜索树结果与实际预期不相等。");
+    }
+
+    @Test
+    public void testLeafNode() {
+        TreeNode root = new TreeNode<>().arrayToTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        TreeNode tree = new Q450DeleteNodeInBST().deleteNode(root, 2);
+
+        Assertions.assertArrayEquals(new Integer[]{5, 3, 6, null, 4, null, 7}, tree.treeToArray(tree, Integer.class),
+                "当为叶子节点时，删除二叉搜索树结果与实际预期不相等。");
+    }
+
+    @Test
+    public void testLeftNull() {
+        TreeNode root = new TreeNode<>().arrayToTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        TreeNode tree = new Q450DeleteNodeInBST().deleteNode(root, 6);
+
+        Assertions.assertArrayEquals(new Integer[]{5, 3, 7, 2, 4}, tree.treeToArray(tree, Integer.class),
+                "当节点左空 & 右非空时，删除二叉搜索树结果与实际预期不相等。");
+    }
+
+    @Test
+    public void testRightNull() {
+        TreeNode root = new TreeNode<>().arrayToTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        TreeNode tree = new Q450DeleteNodeInBST().deleteNode(root, 0);
+
+        Assertions.assertArrayEquals(new Integer[]{5, 3, 6, 2, 4, null, 7}, tree.treeToArray(tree, Integer.class),
+                "当节点左非空 & 右空时，删除二叉搜索树结果与实际预期不相等。");
+    }
+
+    @Test
+    public void testNotNUll() {
+        TreeNode root = new TreeNode<>().arrayToTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        TreeNode tree = new Q450DeleteNodeInBST().deleteNode(root, 3);
+
+        Assertions.assertArrayEquals(new Integer[]{5, 4, 6, 2, null, null, 7}, tree.treeToArray(tree, Integer.class),
+                "当节点左非空 & 右非空时，删除二叉搜索树结果与实际预期不相等。");
+    }
+}
