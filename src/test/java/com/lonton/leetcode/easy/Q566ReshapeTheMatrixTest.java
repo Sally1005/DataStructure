bf736a8403012faee724542e2ea881379d533162
Sally <1564358705@qq.com>
2023-04-25 12:10:40 +0800

    Q566ReshapeTheMatrixTest

---
+++ src/test/java/com/lonton/leetcode/easy/Q566ReshapeTheMatrixTest.java
@@ -0,0 +1,35 @@
+package com.lonton.leetcode.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 566. 重塑矩阵
+ * <ul>
+ * <li> 合理： 输入：mat = [[1,2],[3,4]], r = 1, c = 4,输出：[[1,2,3,4]]；
+ * <li> 不合理：输入：mat = [[1,2],[3,4]], r = 2, c = 4,输出：[[1,2],[3,4]]。
+ * </ul>
+ *
+ * @author 张利红
+ */
+public class Q566ReshapeTheMatrixTest {
+    @Test
+    public void test() {
+        Q566ReshapeTheMatrix matrix = new Q566ReshapeTheMatrix();
+        int[][] arr = new int[][]{{1, 2}, {3, 4}};
+        matrix.matrixReshape(arr, 1, 4);
+        Assertions.assertArrayEquals(
+                new int[][]{{1, 2, 3, 4}}, arr,
+                " 当矩阵重塑合理时，重塑矩阵失败。");
+    }
+
+    @Test
+    public void testNotReasonable() {
+        Q566ReshapeTheMatrix matrix = new Q566ReshapeTheMatrix();
+        int[][] arr = new int[][]{{1, 2}, {3, 4}};
+        matrix.matrixReshape(arr, 2, 4);
+        Assertions.assertArrayEquals(
+                new int[][]{{1, 2}, {3, 4}}, arr,
+                " 当矩阵重塑不合理时，重塑矩阵失败。");
+    }
+}
