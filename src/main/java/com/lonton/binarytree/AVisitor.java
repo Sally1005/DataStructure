468572d61e44f755b3b484ae59ab1e273cc1a7d3
Sally <1564358705@qq.com>
2023-05-06 16:44:51 +0800

    添加过滤器列表，addFilter方法，重写visit方法，实现过滤器功能，以及添加若满足过滤器列表中所有过滤器的条件的抽象方法 doVisit

--- src/main/java/com/lonton/binarytree/AVisitor.java
+++ src/main/java/com/lonton/binarytree/AVisitor.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 /**
@@ -14,43 +15,54 @@ import java.util.function.Predicate;
  */
 
 @Data
-public abstract class AVisitor {
-    /**
-     * 过滤属性
-     */
+public abstract class AVisitor implements IVisitor{
+
+    // 声明一个用于存储过滤器的列表
     private List<Predicate<BinaryTree.TreeNode>> filters = new ArrayList<>();
 
+    // 用于存储找到的树节点
+    protected BinaryTree.TreeNode foundNode;
+
     /**
-     * 添加过滤器
+     * 添加过滤器的方法
+     *
+     * @param filter   要添加到过滤器列表中的过滤器
      */
-    public void addFilter(Predicate<BinaryTree.TreeNode> filter) {
-        filters.add(filter);
+    public  void addFilter(Predicate<BinaryTree.TreeNode> filter){
+            this.filters.add(filter);
     }
 
     /**
-     * 访问树
+     *  重写visit方法，实现过滤器功能（如果满足过滤器列表中所有谓词（过滤器）的条件，则调用抽象方法 doVisit ）
      *
-     * @param node   节点
-     * @param filter 过滤器
-     * @return 任意类型
+     * @param node    要访问的节点
+     * @param param   可选参数列表
+     * @return        抽象方法 doVisit 的返回结果或 null
      */
-    public Object visit(BinaryTree.TreeNode node, Predicate<BinaryTree.TreeNode> filter) {
-        for (Predicate<BinaryTree.TreeNode> f : filters) {
-            // 逐一添加所有过滤器
-            filter = filter.and(f);
+    @Override
+    public Object visit(BinaryTree.TreeNode node, Object... param) {
+        if (filters != null && filters.stream().filter(Objects::nonNull).allMatch(filter -> filter.test(node))) {
+            return doVisit(node, param);
         }
-        if (filter.test(node)) {
-            return doVisit(node);
-        } else {
-            return null;
-        }
+        return null;
     }
 
     /**
-     * 方便重载以便具体实现类返回相应的返回值
+     * 抽象方法，必须由子类实现，用于执行具体的访问操作
      *
-     * @param node 节点
-     * @return 任意类型
+     * @param node    要访问的节点
+     * @param param   可选参数列表
+     * @return        访问结果
      */
-    protected abstract Object doVisit(BinaryTree.TreeNode node);
+    public abstract Object doVisit(BinaryTree.TreeNode node, Object... param);
+
+    /**
+     * 获取在访问期间找到的节点
+     *
+     * @return    在访问期间找到的节点
+     */
+    @Override
+    public BinaryTree.TreeNode getFoundNode() {
+        return foundNode;
+    }
 }
