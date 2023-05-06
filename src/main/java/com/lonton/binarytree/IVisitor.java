f5bfd867087c09de4b185579d1d676e510bd9808
Sally <1564358705@qq.com>
2023-05-06 16:40:57 +0800

    删除大量冗余代码和注释

--- src/main/java/com/lonton/binarytree/IVisitor.java
+++ src/main/java/com/lonton/binarytree/IVisitor.java
@@ -3,13 +3,8 @@ package com.lonton.binarytree;
 
 import com.lonton.binarytree.pojo.BinaryTree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
 /**
  * 二叉树访问数据接口
- * <p/>
  *
  * @author 张利红
  */
@@ -23,25 +18,6 @@ public interface IVisitor {
      * @return 任意类型
      */
     Object visit(BinaryTree.TreeNode node, Object... param);
-//
-//    /**
-//     * 访问树<重载过滤>
-//     *
-//     * @param node      节点
-//     * @param predicate 预测(过滤输入内容是否符合条件)
-//     * @return 任意类型
-//     */
-//    Object visit(BinaryTree.TreeNode node, Predicate<BinaryTree.TreeNode> predicate);
-
-    /**
-     * 访问树 <重载>
-     *
-     * @param node      节点
-     * @param predicate 过滤条件
-     * @return 任意类型
-     */
-    Object visit(BinaryTree.TreeNode node, List<Predicate<BinaryTree.TreeNode>> predicate);
-
 
     /**
      * 查找节点
@@ -49,23 +25,5 @@ public interface IVisitor {
      * @return 已查找到的节点
      */
     BinaryTree.TreeNode getFoundNode();
-
-    /**
-     * 过滤
-     *
-     * @return 满足过滤条件的数据
-     */
-    default List<Predicate<BinaryTree.TreeNode>> predicates() {
-        return new ArrayList<>();
-    }
-
-    //    /**
-//     * 设置默认值为true
-//     *
-//     * @return
-//     */
-//    default Predicate<BinaryTree.TreeNode> predicate() {
-//        return s -> true;
-//    }
 }
 
